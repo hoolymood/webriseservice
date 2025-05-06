4f756fe4500a72779c2d4a2ddce41c0812f2ba9f
Ivan Iashkevich <prvprdk@gmail.com>
2025-05-06 20:34:55 +0300

    Initial commit

---
+++ src/test/java/com/example/webriseservice/WebriseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webriseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebriseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
